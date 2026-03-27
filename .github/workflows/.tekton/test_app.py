7cc7e978570050fdf783d1837b08e1544a4638b3
Madhu Bala Sivakumar <madhusivakumar1@gmail.com>
2026-03-27 13:18:39 +0530

    Create test_app.py

---
+++ .github/workflows/.tekton/test_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
